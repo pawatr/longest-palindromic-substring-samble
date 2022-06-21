7412dcb90e07d90f0596d6d82c0b8a045cbf87b4
Pawat <pawat@apptitude.co.th>
2022-06-21 09:46:49 +0700

    setup project

---
+++ app/src/main/java/com/pawat/longestpalindromic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pawat.longestpalindromic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
